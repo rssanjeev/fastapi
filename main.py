069be56ad45ec56697e292a3f1ee588bb02d3fd7
Sanjeev <35099154+rssanjeev@users.noreply.github.com>
2025-05-18 15:19:47 -0400

    Added find_post method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
+
 app = FastAPI()
 
 class Post(BaseModel):
@@ -17,11 +19,27 @@ async def root():
     return {"message": "Hello"}
 
 @app.get("/posts")
-def get_post():
+def get_posts():
+    print(my_posts)
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    print(post)
-    # return {"New Post": f"Title is {payload['title']}"}
-    return {"New Post": post}
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0,1000000 )
+    my_posts.append(post_dict)
+    return {"New Post": post_dict}
+
+def find_post(id):
+    for p in my_posts:
+        if p['id']==id: return p
+
+@app.get("/posts/{id}")
+def get_post(id:int, response: Response):
+    # print(find_post(id))
+    if not find_post(id): 
+        # response.status_code = status.HTTP_404_NOT_FOUND  
+        # return {"message":f"Post with id {id} was not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Post with id {id} was not found")
+    return {"post details":find_post(id)}
