fab4ec28fbc816a72600597f1f6ce635c6aee6a9
Sanjeev <35099154+rssanjeev@users.noreply.github.com>
2025-05-31 15:21:16 -0400

    Initial Database Connection

--- app/main.py
+++ app/main.py
@@ -2,8 +2,23 @@ from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg2, time
+from psycopg2.extras import RealDictCursor 
+
 
 app = FastAPI()
+
+while True:
+    try:
+        conn = psycopg2.connect(host = 'localhost', database='fastapi', 
+                                user='postgres', password='sarcasm1@123', 
+                                cursor_factory=RealDictCursor, port=5433)  
+        cursor = conn.cursor()
+        print("Successfully connected!")
+        break
+    except Exception as error:
+        print(f"Connection Failed:", error)
+        time.sleep(3)
 
 class Post(BaseModel):
     title: str
