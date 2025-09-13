821d98631dd95a4ce06a173f4b38481d808e7de8
Sanjeev <35099154+rssanjeev@users.noreply.github.com>
2025-09-13 19:44:24 -0400

    Refactor post retrieval and creation to utilize SQLAlchemy and Pydantic schemas for improved data handling

--- app/main.py
+++ app/main.py
@@ -42,20 +42,14 @@ async def root():
 
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
-    # print(my_posts)
-    # cursor.execute(""" SELECT * FROM POSTS """)
-    # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {"data": posts }
+    return posts 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     try:
-        # Filter only model fields
-        # print({key: value for key, value in post.model_dump().items()})
         model_fields = {key: value for key, value in post.model_dump().items()}
         new_post = models.Post(**model_fields)
-        print(new_post)
         db.add(new_post)
         db.commit()
         db.refresh(new_post)
@@ -76,7 +70,7 @@ def get_post(id:int, response: Response, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exists!")
-    return {"post details":post}
+    return post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT ) 
 def delete_post(id:int, db: Session = Depends(get_db)):
@@ -95,7 +89,6 @@ def update_post(id:int, post:schemas.PostUpdate, db: Session = Depends(get_db)):
     if existing_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post is id: {id} deosnt exist!")
-    print(post.model_dump())
     post_query.update(post.model_dump(), synchronize_session=False)
     db.commit()
     return {"message":"Post Updated"}
--- app/schemas.py
+++ app/schemas.py
@@ -1,20 +1,24 @@
 from pydantic import BaseModel
 from typing import Optional
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = 0
+from datetime import datetime
 
 class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = True
+    rating: Optional[int] = 0
 
 class PostCreate(PostBase):
     pass
 
 class PostUpdate(PostBase):
     pass
+
+class PostResponse(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
