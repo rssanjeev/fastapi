3d9d0b5298046062caeec4f18ff3927471d2cb73
Sanjeev <35099154+rssanjeev@users.noreply.github.com>
2025-09-20 09:22:58 -0400

    Implement user login endpoint and define UserLogin schema for authentication

--- app/schemas.py
+++ app/schemas.py
@@ -24,6 +24,10 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
+class UserLogin (BaseModel):
+    email: EmailStr
+    password: str
+
 class UserOut(BaseModel):
     id: int
     email: EmailStr
