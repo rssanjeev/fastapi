342c29a6f3d6ca0c1dd44e8ed5c5351802380f78
Sanjeev <35099154+rssanjeev@users.noreply.github.com>
2025-09-21 10:20:43 -0400

    Refactor login function to improve password verification and add OAuth2 token creation utility

--- app/utils.py
+++ app/utils.py
@@ -5,5 +5,5 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash(password: str) -> str:
     return pwd_context.hash(password)
 
-def verify(password: str, hashed: str) -> bool:
-    return pwd_context.verify(password, hashed)  
+def verify(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)   
